8d03d6a2e095c4d4ec6bb8cab3e294591e8aa0a8
lemniscate77 <lemniscate77@users.noreply.github.com>
2020-08-08 11:54:42 +0300

    мое решение 2.1.4 от 08.08.2020

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    public final Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
